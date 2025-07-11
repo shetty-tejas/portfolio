ce8f56d2184689f56a47c903243abeef26aa7a7b
Tejas <tejas.shetty@mailbox.org>
2025-07-12 02:41:24 +0530

    Initial Design 🚀

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,140 @@
+<script module>
+	import '../app.css';
+</script>
+
+<script lang="ts">
+	import type { Component, Snippet } from 'svelte';
+
+	import { CopyrightIcon, GithubIcon, InstagramIcon, LinkedinIcon, MailIcon } from '@lucide/svelte';
+
+	import { page } from '$app/state';
+	import { cn } from '$lib/utils';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	type NavLink = {
+		label: string;
+		href: string;
+	};
+
+	type FooterLink = {
+		href: string;
+		icon: Component;
+
+		target?: string;
+	};
+
+	const navLinks: Array<NavLink> = [
+		{
+			label: 'Home',
+			href: '/'
+		},
+		{
+			label: 'Blogs',
+			href: '/blogs'
+		},
+		{
+			label: 'Chapters',
+			href: '/chapters'
+		},
+		{
+			label: 'Contact',
+			href: '/contact'
+		}
+	];
+
+	const footerLinks: Array<FooterLink> = [
+		{
+			href: 'mailto:tejas.shetty@mailbox.org',
+			icon: MailIcon
+		},
+		{
+			href: 'https://github.com/shetty-tejas',
+			target: '_blank',
+			icon: GithubIcon
+		},
+		{
+			href: 'https://linkedin.com/in/shetty-tejas',
+			target: '_blank',
+			icon: LinkedinIcon
+		},
+		{
+			href: 'https://instagram.com/_shettytejas_',
+			target: '_blank',
+			icon: InstagramIcon
+		}
+	];
+
+	let { children }: Props = $props();
+
+	function isNavLinkActive(link: NavLink): boolean {
+		const path = page.url.pathname;
+
+		if (link.href === '/') return path === link.href;
+
+		return path.startsWith(link.href);
+	}
+</script>
+
+<div class="max-w-5xl mx-4 lg:mx-auto mb-8 md:mb-40 mt-8 md:mt-20 lg:mt-40">
+	<div class="flex flex-col md:flex-row gap-y-4">
+		<div class="space-y-3">
+			<aside class="min-w-50 font-decor space-y-4 md:space-y-8 md:sticky md:top-24 select-none">
+				<div class="flex flex-col gap-y-8">
+					<a href="/" class="w-fit">
+						<img src="/logo.svg" alt="TS" class="size-8 md:size-12 rounded-full" />
+					</a>
+					<span class="font-decor font-bold text-lg tracking-[0.075rem] uppercase md:hidden"
+						>Tejas Shetty</span
+					>
+				</div>
+
+				<nav class="flex md:flex-col gap-x-6 gap-y-1.5 font-light text-sm">
+					{#each navLinks as link (link.href)}
+						{@const isActive = isNavLinkActive(link)}
+
+						<div
+							class="flex max-md:flex-col-reverse max-md:justify-end items-center gap-x-1.5 gap-y-1"
+						>
+							<div class={cn('size-1.25 bg-mocha-peach rounded-full', { hidden: !isActive })}></div>
+							<a
+								href={link.href}
+								class={isActive ? 'text-neutral font-medium' : 'text-subtext hover:text-neutral'}
+								>{link.label}</a
+							>
+						</div>
+					{/each}
+				</nav>
+			</aside>
+
+			<div class="border-t border-surface-lighter md:hidden"></div>
+		</div>
+
+		<div class="flex-1 flex flex-col gap-y-16 pt-2">
+			<header class="hidden md:block select-none">
+				<h1 class="font-decor font-bold text-xl tracking-[0.075rem] uppercase">Tejas Shetty</h1>
+			</header>
+
+			<main class="font-body flex-1">
+				{@render children()}
+			</main>
+
+			<footer class="flex justify-between mt-8 md:mt-28 select-none">
+				<span class="flex items-center gap-x-1 text-sm text-subtle">
+					<CopyrightIcon size="16" />
+					{new Date().getFullYear()} Tejas Shetty.
+				</span>
+
+				<span class="flex items-center gap-x-2">
+					{#each footerLinks as link (link.href)}
+						<a href={link.href} target={link.target} class="text-subtle hover:text-neutral">
+							<link.icon size="16" />
+						</a>
+					{/each}
+				</span>
+			</footer>
+		</div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+	<title>Hey! 👋🏾</title>
+</svelte:head>
+
+<div class="tracking-wide font-light leading-relaxed space-y-4">
+	<p>
+		I'm a developer based in Mumbai, India. I currently work at <a
+			href="https://www.sage.com"
+			target="_blank">Sage</a
+		>
+		as a <strong>Senior Engineer</strong>, where I’m part of the team building
+		<a href="https://www.sage.com/en-us/sage-network/" target="_blank">Sage Network</a>
+		and the
+		<a href="https://network.sage.com/e-invoicing/register/" target="_blank"
+			>Sage E-Invoicing Portal</a
+		>.
+	</p>
+
+	<p>
+		After hours, I like to tinker with ideas and build things I enjoy. Lately, that means working on
+		a few SaaS products using <strong>Ruby on Rails</strong> and <strong>SvelteKit</strong>, diving
+		into <strong>Go</strong>, and exploring deeper engineering topics like building interpreters and
+		writing concurrent applications. I also try to contribute to <strong>open-source</strong> whenever
+		I can — PRs, fixes, small features, anything that helps.
+	</p>
+
+	<!-- TODO: Find a way to add a book name dynamically -->
+
+	<p>
+		When I’m not coding, you’ll usually find me <a
+			href="https://www.strava.com/athletes/110074637"
+			target="_blank">running, biking, or swimming</a
+		>, i.e. basically hustling towards becoming a <strong>triathlete</strong>! I also enjoy reading
+		sometimes, and when I’m feeling especially lazy... I just binge-watch YouTube!
+	</p>
+</div>
+
+<style lang="postcss">
+	@reference "../app.css";
+
+	a,
+	strong {
+		@apply font-medium;
+	}
+
+	a {
+		@apply text-link underline;
+	}
+</style>
